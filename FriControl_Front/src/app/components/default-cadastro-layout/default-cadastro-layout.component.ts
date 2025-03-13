ca93f4229b0ebb7ef1ea48799378d680148e7b39
jeanv1ctor <jean2563victor@gmail.com>
2025-03-13 16:49:54 -0300

    Commit inicial do front end

---
+++ FriControl_Front/src/app/components/default-cadastro-layout/default-cadastro-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-default-cadastro-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './default-cadastro-layout.component.html',
+  styleUrl: './default-cadastro-layout.component.scss'
+})
+export class DefaultCadastroLayoutComponent {
+
+}
