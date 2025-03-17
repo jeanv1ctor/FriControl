1772d28b3ea6daf0d6eb525e30723c2ee75593ae
jeanv1ctor <jean2563victor@gmail.com>
2025-03-17 15:34:53 -0300

    feat: criando e estilizando a rota de login

---
+++ FriControl_Front/src/app/components/button/button.component.ts
@@ -0,0 +1,15 @@
+import { NgClass } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [NgClass],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss'
+})
+export class ButtonComponent {
+
+  @Input() textButton: string;
+  @Input() size: string = 'auto';
+}
