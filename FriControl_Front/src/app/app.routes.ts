cd25587d92ed500bfee6173a08e39724018a4fdd
jeanv1ctor <jean2563victor@gmail.com>
2025-03-17 18:51:47 -0300

    refactor: melhorando a validacao para acessar a rota home

--- FriControl_Front/src/app/app.routes.ts
+++ FriControl_Front/src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AuthGuardService } from './services/auth-guard.service';
 
 export const routes: Routes = [
     {path: '', component: LoginComponent},
-    {path: 'login', component: LoginComponent}
+    {path: 'login', component: LoginComponent},
+    {path: 'home', component: HomeComponent, canActivate: [AuthGuardService]}
 ];
--- FriControl_Front/src/app/pages/login/login.component.ts
+++ FriControl_Front/src/app/pages/login/login.component.ts
@@ -3,6 +3,7 @@ import { ButtonComponent } from '../../components/button/button.component';
 import { FormsModule } from '@angular/forms';
 import { LoginService } from '../../services/login.service';
 import { ToastrService } from 'ngx-toastr';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -23,12 +24,16 @@ export class LoginComponent {
 
   constructor(
     private loginService: LoginService,
-    private toastService: ToastrService
+    private toastService: ToastrService,
+    private rota: Router
   ){}
 
   login(){
     this.loginService.autentica(this.email, this.senha).subscribe({
-      next: () => this.toastService.success("Usuario autenticado!"),
+      next: () => {
+        this.toastService.success("Usuario autenticado!"),
+        this.rota.navigate(['home']);
+      },
       error: () =>  this.toastService.error("Falha ao autenticar usuario!")
     })
   }
